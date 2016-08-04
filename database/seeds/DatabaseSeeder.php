b3334a8fd5bbb8e8273ce1afa48b6e9bf470d491
vagrant <vagrant@homestead>
2016-08-04 20:03:55 +0000

    working just needed to reload class map

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // Model::unguard();
         $this->call('BreedsTableSeeder');
     }
 }
