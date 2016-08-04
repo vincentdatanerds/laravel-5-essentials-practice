03224cce39ae31fb44636fd329b48225b2984f08
vagrant <vagrant@homestead>
2016-08-04 22:29:09 +0000

    app broken now after adding html collective

---
+++ resources/views/partials/forms/cat.blade.php
@@ -0,0 +1,10 @@
+
+
+<div class="form-group">
+	{!! Form:label('name', 'Name') !!}
+	<div class="form controls">
+		{!! Form::text('name', null, ['class' => 'form-control']) !!}
+	</div>
+
+
+</div>
