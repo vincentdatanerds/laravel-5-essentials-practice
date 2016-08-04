99e3f91976fdb085113aecc31ef2bc53ea1ec154
vagrant <vagrant@homestead>
2016-08-04 20:27:25 +0000

    add css files

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Furbook</title>
+		<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
+	</head>
+	<body>
+		<div class="container">
+			<div class="page-header">
+				@yield('header')
+			</div>
+
+			@if (Session::has('success'))
+				<div class="alert alert-success">
+					{{ Session::get('success') }}
+				</div>
+			@endif
+			@yield('content')
+		</div>
+	</body>
+</html>
