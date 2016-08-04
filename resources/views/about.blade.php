8c00a5aa5744ad620bfce7937d742ef348f9b2dc
vagrant <vagrant@homestead>
2016-08-04 20:40:38 +0000

    finished master layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('header')
+	<h2>About this site</h2>
+
+@stop
+
+
+@section('content')
+	<p>There are over {{ $number_of_cats }} cat on this site!</p>
+
+@stop
--- resources/views/about.php
+++
@@ -1,2 +0,0 @@
-<h2> About this site </h2>
-There are over <?php echo $number_of_cats; ?> cats on this site!
