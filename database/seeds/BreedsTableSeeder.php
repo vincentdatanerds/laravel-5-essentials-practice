88acb067f84c336ee20ed972ed01e063d01820f3
vagrant <vagrant@homestead>
2016-08-04 19:58:26 +0000

    added db seeder for breeds

---
+++ database/seeds/BreedsTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class BreedsTableSeeder extends Seeder {
+	public function run() {
+		DB::table('breeds') -> insert([
+			['id' => 1, 'name' => 'Domestic'],
+			['id' => 2, 'name' => 'Persian'],
+			['id' => 3, 'name' => 'Siamese'],
+			['id' => 4, 'name' => 'Abyssinian'],
+		]);
+	}
+}
