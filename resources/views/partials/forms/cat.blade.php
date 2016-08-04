71c2b6520c3976716aa1ce7268afc782eab3936a
vagrant <vagrant@homestead>
2016-08-04 22:39:43 +0000

    adding fields to form

--- resources/views/partials/forms/cat.blade.php
+++ resources/views/partials/forms/cat.blade.php
@@ -2,9 +2,17 @@
 
 <div class="form-group">
 	{!! Form:label('name', 'Name') !!}
-	<div class="form controls">
+	<div class="form-controls">
 		{!! Form::text('name', null, ['class' => 'form-control']) !!}
 	</div>
-
-
 </div>
+
+
+<div class="form-group">
+{!! Form::label('date_of_birth', 'Date of birth') !!}
+	<div class="form-controls">
+		{{!! Form::date('date_of_birth', null, ['class' => 'form-control']) !!}}
+	</div>
+</div>
+
+
