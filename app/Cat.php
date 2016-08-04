dedaa62271d511b009b07ace59f711cf965e5cb3
vagrant <vagrant@homestead>
2016-08-04 18:22:11 +0000

    added db migrations

--- app/Cat.php
+++ app/Cat.php
@@ -4,8 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cat extends Model {
 	protected $fillable = ['name', 'date_of_birth', 'breed_id'];
-	public function() breed(){
+	public function breed(){
 		return $this -> belongsTo('Furbook\Breed');
 
-	};
+	}
 }
