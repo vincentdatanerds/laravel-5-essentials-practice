ddd35fae964cea5f4d0f2b7574503b28f0520d4f
vagrant <vagrant@homestead>
2016-08-05 18:18:47 +0000

    cleaned up routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,49 +11,16 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-//Route::get('/', function(){
-//	// return 'All cats';
-//	return redirect('cats');
-//});
-//
-//
 Route::get('cats', function(){
 	$cats = Furbook\Cat::all();
 	return view('cats.index')->with('cats', $cats);
 });
-//
-//
-//Route::get('cats/{cat}', function(Furbook\Cat $cat){
-//	// $cat = Furbook\Cat::find($id);
-//	return view('cats.show') -> with('cat', $cat);
-//});
-
-//Route::put('cats/{cat}')
-
-// Route::get('cats/{id}', function($id){
-// 	$cat = Furbook\Cat::find($id);
-// 	return view('cats.show') -> with('cat', $cat);
-// });
-
-
-//
-//Route::get('about', function(){
-//	//return 'Hi';
-//	return view('about') -> with('total_number_of_cats', 440);
-//});
-
 
 
 Route::group(['prefix' => 'cats'], function () {
-
    Route::get('create', function(){
  	 return view('cats.create');
    });
-
 });
 
 Route::post('cats', function(){
@@ -61,35 +28,5 @@ Route::post('cats', function(){
     return redirect('cats/'.$cat->id)
         ->withSuccess('Cat has been created.');
 });
-
-
-
-
-// Route::get('cats/breeds/{name}', function(){
-// 	$breed = Furbook\Breed::with('cats')
-// 		->whereName($name)
-// 		->first();
-// 	return view('cats.index')
-// 		->with('breed', $breed)
-// 		->with('cats', $breed->cats);
-
-
-// });
-
-
-
-// Route::get('cats/create', function(){
-// 	return "hi";
-// 	// return view('cats.index');
-// 	return view('about') -> with('total_number_of_cats', 440);
-// });
-
-
-//
-//Route::get('hello', function(){
-//		return view('about') -> with('total_number_of_cats', 440);
-//
-//});
-
 
 
