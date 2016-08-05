05f6dd677688d490ccd822b92fba3dc17b8d1073
vagrant <vagrant@homestead>
2016-08-05 21:06:47 +0000

    can't edit cat

--- resources/views/cats/edit.blade.php
+++ resources/views/cats/edit.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('content')
-    {!! Form::model($cat, ['url' => '/cats/'.$cat->id], ['method' => 'put']) !!}
+    {!! Form::model($cat, ['url' => '/cats/'.$cat->id]) !!}
     @include('partials.forms.cat')
     {!! Form::close() !!}
 @stop
