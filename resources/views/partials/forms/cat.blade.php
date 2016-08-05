45624577f6dd2d9b0232c33bb3f99a76c6a05ffd
vagrant <vagrant@homestead>
2016-08-05 17:22:25 +0000

    forms work now

--- resources/views/partials/forms/cat.blade.php
+++ resources/views/partials/forms/cat.blade.php
@@ -1,5 +1,3 @@
-
-
 <div class="form-group">
 	{!! Form::label('name', 'Name') !!}
 	<div class="form-controls">
@@ -7,13 +5,18 @@
 	</div>
 </div>
 
-
 <div class="form-group">
-{!! Form::label('date_of_birth', 'Date of birth') !!}
+	{!! Form::label('date_of_birth', 'Date of Birth') !!}
 	<div class="form-controls">
 		{!! Form::date('date_of_birth', null, ['class' => 'form-control']) !!}
 	</div>
 </div>
 
+<div class="form-group">
+	{!! Form::label('breed_id', 'Breed') !!}
+	<div class="form-controls">
+		{!! Form::select('breed_id', $breeds, null, ['class' => 'form-control']) !!}
+	</div>
+</div>
 
-{!! Form::submit('Save Cat', ['class' => 'btn btn-primary'] !!}
+{!! Form::submit('Save Cat', ['class' => 'btn btn-primary']) !!}
