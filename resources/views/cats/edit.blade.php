afb8cc68a21593d9f7cef4c16c2c2c568b6bdb53
vagrant <vagrant@homestead>
2016-08-05 20:48:51 +0000

    fixed bug from book

--- resources/views/cats/edit.blade.php
+++ resources/views/cats/edit.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.master')
+
 @section('header')
-
-    <h2>EDit a cat</h2>
-
-
+    <h2>Edit a cat</h2>
 @stop
-
 
 @section('content')
-    {!! Form::model($cat, ['url' => '/cats/'.$cat->id], 'method' => 'put') !!}
-        @include('partials.forms.cat')
+    {!! Form::model($cat, ['url' => '/cats/'.$cat->id], ['method' => 'put']) !!}
+    @include('partials.forms.cat')
     {!! Form::close() !!}
 @stop
+
+
+
+
