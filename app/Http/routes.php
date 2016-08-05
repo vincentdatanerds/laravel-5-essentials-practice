b2fae182094a3cd871da115c49c618e236b78f25
vagrant <vagrant@homestead>
2016-08-05 16:55:44 +0000

    fixed cat create route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,22 +15,22 @@
 //     return view('welcome');
 // });
 
-Route::get('/', function(){
-	// return 'All cats';
-	return redirect('cats');
-});
-
-
-Route::get('cats', function(){
-	$cats = Furbook\Cat::all();
-	return view('cats.index')->with('cats', $cats);
-});
-
-
-Route::get('cats/{cat}', function(Furbook\Cat $cat){
-	// $cat = Furbook\Cat::find($id);
-	return view('cats.show') -> with('cat', $cat);
-});
+//Route::get('/', function(){
+//	// return 'All cats';
+//	return redirect('cats');
+//});
+//
+//
+//Route::get('cats', function(){
+//	$cats = Furbook\Cat::all();
+//	return view('cats.index')->with('cats', $cats);
+//});
+//
+//
+//Route::get('cats/{cat}', function(Furbook\Cat $cat){
+//	// $cat = Furbook\Cat::find($id);
+//	return view('cats.show') -> with('cat', $cat);
+//});
 
 
 // Route::get('cats/{id}', function($id){
@@ -39,38 +39,47 @@ Route::get('cats/{cat}', function(Furbook\Cat $cat){
 // });
 
 
+//
+//Route::get('about', function(){
+//	//return 'Hi';
+//	return view('about') -> with('total_number_of_cats', 440);
+//});
 
-Route::get('about', function(){
-	//return 'Hi';
-	return view('about') -> with('total_number_of_cats', 440);
-});
 
 
-Route::get('cats/breeds/{name}', function(){
-	$breed = Furbook\Breed::with('cats')
-		->whereName($name)
-		->first();
-	return view('cats.index')
-		->with('breed', $breed)
-		->with('cats', $breed->cats);
+Route::group(['prefix' => 'cats'], function () {
 
+   Route::get('create', function(){
+ 	 return view('cats.index');
+   });
 
 });
 
+// Route::get('cats/breeds/{name}', function(){
+// 	$breed = Furbook\Breed::with('cats')
+// 		->whereName($name)
+// 		->first();
+// 	return view('cats.index')
+// 		->with('breed', $breed)
+// 		->with('cats', $breed->cats);
 
 
-Route::get('cats/create', function(){
-	// return view('cats.index');
-	return view('about') -> with('total_number_of_cats', 440);
-});
+// });
 
 
 
-Route::get('hello', function(){
-		return view('about') -> with('total_number_of_cats', 440);
+// Route::get('cats/create', function(){
+// 	return "hi";
+// 	// return view('cats.index');
+// 	return view('about') -> with('total_number_of_cats', 440);
+// });
 
 
-});
+//
+//Route::get('hello', function(){
+//		return view('about') -> with('total_number_of_cats', 440);
+//
+//});
 
 
 
