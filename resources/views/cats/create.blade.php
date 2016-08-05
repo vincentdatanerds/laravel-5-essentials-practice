79f6f7c0d2cc4f07df72bde99ee9e793fb995e83
vagrant <vagrant@homestead>
2016-08-05 17:05:48 +0000

    preparing to refactor composer folder to composers

--- resources/views/cats/create.blade.php
+++ resources/views/cats/create.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.master')
 @section('header')
-{{-- 	<h2>Add a new cat</h2>
+ 	<h2>Add a new cat</h2>
 @stop
 @section('content')
 
 {!! Form::open(['url' => '/cats']) !!}
+    @include('partials.forms.cat')
 
-
-{!! Form::close() !!} --}}
+{!! Form::close() !!}
 
 @stop
